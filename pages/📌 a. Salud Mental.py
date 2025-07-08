fd4b02979c7935161a6d5937b64ced7b4b6c40c6
CESARSERNA29 <79622570+CESARSERNA29@users.noreply.github.com>
2025-07-08 01:04:43 -0500

    Add files via upload

--- pages/📌 a. Salud Mental.py
+++ pages/📌 a. Salud Mental.py
@@ -174,7 +174,8 @@ with Tab1:
     #---------------------------------------------------------------------------   
     st.markdown("<h4 style='color:#547FD4; font-weight:bold;'>Resumen Tabular del grupo de Enfermedades:</h4>", unsafe_allow_html=True)
     tabla_sm1 = pd.pivot_table(df_sm0, values='anio', index='grupo', aggfunc='count', fill_value=0).reset_index() 
-    tabla_sm1.rename(columns={'anio': 'cant'}, inplace=True)
+    # tabla_sm1.rename(columns={'anio': 'cant'}, inplace=True)
+    tabla_sm1.columns = ['anio', 'cant']
 
     
     # Calcular el total de casos 
