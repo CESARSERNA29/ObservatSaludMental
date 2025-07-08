50165ef2bb448c5e425d03a48b53573424e96614
CESARSERNA29 <79622570+CESARSERNA29@users.noreply.github.com>
2025-07-08 00:46:18 -0500

    Add files via upload

--- pages/📌 a. Salud Mental.py
+++ pages/📌 a. Salud Mental.py
@@ -174,14 +174,14 @@ with Tab1:
     # Salud mental
     #---------------------------------------------------------------------------   
     st.markdown("<h4 style='color:#547FD4; font-weight:bold;'>Resumen Tabular del grupo de Enfermedades:</h4>", unsafe_allow_html=True)
-    tabla_sm1 = pd.pivot_table(df_sm0, values='anio', index='grupo', aggfunc='count', fill_value=0) 
-    tabla_sm1.rename(columns={'anio': 'cant'}, inplace=True)
+    tabla_sm1 = pd.pivot_table(df_sm0, values='anio', index='grupo', aggfunc='count', fill_value=0)
+    tabla_sm1.rename(columns={'anio': 'cant'}, inplace=True) 
     
-    # Calcular el total de casos
-    # Calcular total general  
-    total_casos = tabla_sm1['cant'].len() 
+    # Calcular el total de casos 
+    # Calcular total general 
+    total_casos = tabla_sm1['anio'].len() 
     # Calcular porcentaje 
-    tabla_sm1['(%)'] = (tabla_sm1['cant'] / total_casos * 100).round(2) 
+    tabla_sm1['(%)'] = (tabla_sm1['anio'] / total_casos * 100).round(2) 
     
     # Agregar fila de totales 
     fila_total = pd.DataFrame({'cant': [total_casos], '(%)': [100.0]}, index=['Total']) 
