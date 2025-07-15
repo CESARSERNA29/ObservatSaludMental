3a9745b00dbeb6a73be64332bf384dac89fd99c7
CESARSERNA29 <79622570+CESARSERNA29@users.noreply.github.com>
2025-07-14 20:13:12 -0500

    Add files via upload

--- pages/📌 a. Salud Mental.py
+++ pages/📌 a. Salud Mental.py
@@ -204,25 +204,19 @@ with Tab1:
             "Selecciona el Grupo de Enfermedad", 
             options = df_sm0["grupo"].unique(), 
             default = df_sm0["grupo"].unique(),
-            ) 
+            )    
     
+    # -----------
     
-    # ----------- 
+    df_selection = df_sm0.query(
+        "departamento==@Departamento & municipio==@Municipio & grupo ==@Grupo"
+    )
     
-    df_selection = df_sm0.query("departamento==@Departamento & municipio==@Municipio & grupo ==@Grupo")
-    
-    # Esta función realiza análisis descriptivos básicos como media, moda, suma, etc. 
-    
+    # Esta función realiza análisis descriptivos básicos como media, moda, suma, etc.
     def Home(): 
         with st.expander("Ver el Conjunto de Datos en Excel"): 
-            showData=st.multiselect('Filter: ',df_selection.columns,
-                                    default=["anio", "sexo", "nombre_cat_edad", 
-                                             "departamento", "municipio", "componente", 
-                                             "capitulo", "grupo", "Enfermedad_Evento", 
-                                             "pob10", "tasa_morb", "Tot_Eventos"]) 
-            st.dataframe(df_selection[showData],use_container_width=True) 
-    
-    
+            showData=st.multiselect('Filter: ',df_selection.columns,default=["anio", "sexo", "nombre_cat_edad", "departamento", "municipio", "componente", "capitulo", "grupo", "Enfermedad_Evento", "pob10", "tasa_morb", "Tot_Eventos"])
+        st.dataframe(df_selection[showData],use_container_width=True)
     # calcular los análisis:
     total_investment = float(pd.Series(df_selection['Tot_Eventos']).sum())
     investment_mode1 = float(pd.Series(df_selection['departamento']).nunique())
@@ -249,10 +243,8 @@ with Tab1:
     #variable distribution Histogram
     #with st.expander("Distribución de Frecuencias - Variables Cuantitativas"):
     # df.hist(figsize=(16,8),color='#898784', zorder=2, rwidth=0.9,legend = ['tasa_morb']);
-    # st.pyplot() 
-    
-    
-    # -----------------------------------------------------
+    # st.pyplot()
+# -----------------------------------------------------
     
     st.markdown("##")
         
