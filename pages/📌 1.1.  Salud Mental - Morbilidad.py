e1108f807d4a1ceba91f017bdfc57314d2f1eb96
CESARSERNA29 <79622570+CESARSERNA29@users.noreply.github.com>
2025-07-20 20:37:57 -0500

    Add files via upload

--- pages/📌 1.1.  Salud Mental - Morbilidad.py
+++ pages/📌 1.1.  Salud Mental - Morbilidad.py
@@ -1152,7 +1152,7 @@ if selected == "📍 Mapa":
     # 1. Cargar la tabla con tasas y el geopandas:
     # Leer shapefile
     #gdf_municipios = gpd.read_file("ciudades_shp/MGN_ADM_MPIO_GRAFICO.shp")
-    gdf_municipios = gpd.read_file("ciudades_shp/municipios_orinoquia.geojson")
+    gdf_municipios = gpd.read_file("municipios_orinoquia.geojson")
 
     df_tasas = pd.read_excel("ciudades_shp/Tabla_Muni_Orinoquia_Mapas_Tasas.xlsx")
     
