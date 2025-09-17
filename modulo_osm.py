5a1048b951163cf4d954a92fe144532d91ac392a
FERNANDO ALONSO VELEZ <fvelez78@yahoo.es>
2025-09-17 00:12:39 -0500

    Update modulo_osm.py

--- modulo_osm.py
+++ modulo_osm.py
@@ -330,7 +330,7 @@ def diag_barras(df, vx, vy, titulo, subt, colores=None, xlab="", ylab="", width=
 def mapa_crp(df,vy,ruta_geoj,Medida):
   
   # se lee el archivo simple con el listado de municipios de la region
-  mpio_orinoquia=pd.read_csv("D:/Consultoria/UNILLANOS/ObservatSaludMental/data/municipios.csv",sep=',')
+  mpio_orinoquia=pd.read_csv("data/municipios.csv",sep=',')
   mpio_orinoquia['id_mpio']=mpio_orinoquia['id_mpio'].astype('Int64')
   
   # Lectura del archivo en formato GeoJson
@@ -626,3 +626,4 @@ def G_Disp3(x, y, nombres):
     )
     
     return fig
+
