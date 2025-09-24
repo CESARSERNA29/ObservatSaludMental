ab5d1f5f8b756c997b3f42a3113b175a25b25e4e
CESARSERNA29 <79622570+CESARSERNA29@users.noreply.github.com>
2025-09-23 23:21:08 -0500

    Add files via upload

--- pages/2_📌 1.1. Salud Mental - Morbilidad.py
+++ pages/2_📌 1.1. Salud Mental - Morbilidad.py
@@ -920,30 +920,30 @@ df = load_data3()
 st.markdown("<h5 style='font-weight:bold;'>📈 Tabla de Frecuencias por Categoría de Edad</h5>", unsafe_allow_html=True)
 
 # Calcular frecuencias
-#frequency = df.nombre_cat_edad.value_counts().sort_index()
-#percentage_frequency = frequency / len(df.nombre_cat_edad) * 100
-#cumulative_frequency = frequency.cumsum()
-#relative_frequency = frequency / len(df.nombre_cat_edad)
-#cumulative_relative_frequency = relative_frequency.cumsum()
+frequency = df.nombre_cat_edad.value_counts().sort_index()
+percentage_frequency = frequency / len(df.nombre_cat_edad) * 100
+cumulative_frequency = frequency.cumsum()
+relative_frequency = frequency / len(df.nombre_cat_edad)
+cumulative_relative_frequency = relative_frequency.cumsum()
 
 # ========================
 # Suma condicionada
 # ========================
-frequency = df.groupby("nombre_cat_edad")["Tot_Eventos"].sum().sort_index()
+#frequency = df.groupby("nombre_cat_edad")["Tot_Eventos"].sum().sort_index()
 
 # Total para calcular porcentajes
-total = frequency.sum()
+#total = frequency.sum()
 
 # Porcentajes
-percentage_frequency = (frequency / total * 100).round(2)
+#percentage_frequency = (frequency / total * 100).round(2)
 
 # Frecuencia acumulada (suma)
-cumulative_frequency = frequency.cumsum()
+#cumulative_frequency = frequency.cumsum()
 
-relative_frequency = (frequency / total).round(2)
+#relative_frequency = (frequency / total).round(2)
 
 # Frecuencia relativa acumulada
-cumulative_relative_frequency = (percentage_frequency.cumsum()).round(2)
+#cumulative_relative_frequency = (percentage_frequency.cumsum()).round(2)
 
 
 
